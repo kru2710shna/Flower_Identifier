146a3e61aefdddee6e9bd9e0b32475be3ac99f02
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-02-06 00:16:03 -0800

    App Running

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import numpy as np
-from flask import request, jsonify  render_template
+from flask import Flask
+from flask import request, jsonify  ,render_template
 import pickle 
 # create app name 
 app = Flask(__name__)
 
 #load the picke model 
-model = pickle.load(open("modeol.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def Home():
     return render_template("index.html")
 
-@app.route("/predict", method = ["POST"])
+@app.route("/predict", methods = ["POST"])
 def Predict():
     float_features = [float(c) for c in request.form.values()]
     features = [np.array(float_features)]
