f55d2365502849a8a3ac868ab6cdfd5c474f274e
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-02-06 00:03:34 -0800

    Flask code created

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import request, jsonify  render_template
+import pickle 
+# create app name 
+app = Flask(__name__)
+
+#load the picke model 
+model = pickle.load(open("modeol.pkl", "rb"))
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/predict", method = ["POST"])
+def Predict():
+    float_features = [float(c) for c in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    
+    return render_template("index.html", prediction_text= "The flower is {}".format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
